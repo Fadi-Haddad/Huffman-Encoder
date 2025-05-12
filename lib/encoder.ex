3563a79fdb0914268556bccb968ca69e96cd6cb3
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2025-05-12 19:21:11 +0300

    Implemented generate_codes function to generate Huffman codes

--- lib/encoder.ex
+++ lib/encoder.ex
@@ -1,5 +1,5 @@
 defmodule Huffman.Encoder do
-  alias Huffman.{Counter, Queue}
+  alias Huffman.{Counter, Queue, Tree}
   # import
   @moduledoc """
   Counter Module is responsible for counting the characters inside the file.
@@ -17,8 +17,23 @@ defmodule Huffman.Encoder do
   def encode(path_to_file) do
     tree = Counter.count_from_file(path_to_file)
             |> Queue.build_queue()
-            # |> Tree.build_leaves()
-            # |> Tree.build_tree()
-
+            |> Tree.build_leaves()
+            |> Tree.build_tree()
+    tree
   end
+
+  def generate_codes(tree) do
+    generate_codes(tree, "")
+  end
+
+  defp generate_codes(%Huffman.Leaf{char: char}, prefix) do
+    %{char => prefix}
+  end
+  
+  defp generate_codes(%Huffman.Node{left: left, right: right}, prefix) do
+    left_codes = generate_codes(left, prefix <> "0")
+    right_codes = generate_codes(right, prefix <> "1")
+
+    Map.merge(left_codes, right_codes)
+end
 end
