e20aea7caef0a3dfc01627f53ace0dbb402195d5
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2025-05-14 23:40:52 +0300

    Added documentation to Huffman facade module

--- lib/huffman.ex
+++ lib/huffman.ex
@@ -1,15 +1,26 @@
 defmodule Huffman do
   @moduledoc """
-  Documentation for `Huffman`.
+  Huffman is the public interface to the Huffman compression system.
+  This module acts as a facade, exposing high-level functions.
+
+  - Encoding files into Huffman-compressed bitstrings
+  - Serializing those bitstrings into binary form
+  - Writing the encoded data (including metadata) to disk
+
+  Internally, it delegates to other modules like `Huffman.Encoder` and `Huffman.Binary`
   """
 
   @doc """
-  Hello world.
+  Encodes the content of the given file using Huffman encoding.
+
+  Returns a tuple `{bitstring, tree}` where:
+  - `bitstring` is a string of `"0"` and `"1"` characters
+  - `tree` is the Huffman tree used for encoding (needed for decoding)
 
   ## Examples
 
-      iex> Huffman.hello()
-      :world
+      iex> Huffman.encode("path/to/input.txt")
+      "0000000100000000010101000100100000000000000010"
 
   """
   alias Huffman.{Binary, Encoder}
@@ -18,13 +29,39 @@ defmodule Huffman do
     Encoder.encode(path_to_file)
   end
 
+  @doc """
+  Serializes a bitstring (string of `"0"` and `"1"` characters) into binary format.
+
+  Returns a tuple `{binary, padding}`:
+  - `binary`: the actual binary data
+  - `padding`: zero-padding added to ensure byte alignment
+
+  ## Examples
+
+      iex> Huffman.serialize_bitstring("010101011010100110")
+      {<<85, 169, 128>>, "000000"}
+
+  """
   def serialize_bitstring(bitstring) do
     Binary.serialize_bitstring(bitstring)
   end
 
+  @doc """
+    Encodes a file and writes the compressed binary to disk.
+
+    The output file will contain:
+    - A 4-byte header size
+    - A serialized header (with the Huffman tree and padding)
+    - The compressed binary data
+    ## Examples
+
+        iex> Huffman.encode_file("input.txt", "output.huff")
+        :ok
+
+  """
   def encode_file(path_to_file, destination_file_name) do
-    {bitstring, tree} = Encoder.encode(path_to_file)
-    {binary_data, padding} = Binary.serialize_bitstring(bitstring)
+    {bitstring, tree} = encode(path_to_file)
+    {binary_data, padding} = serialize_bitstring(bitstring)
     header = %{tree: tree, padding: padding}
     header_binary = :erlang.term_to_binary(header)
     header_size = byte_size(header_binary)
